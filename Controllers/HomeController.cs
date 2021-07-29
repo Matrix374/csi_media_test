7de04858891d3497dce83e2c4cb974b9b56eaaf8
Matrix374 <rumahirfan@gmail.com>
2021-07-29 11:55:55 +0100

    API: Implemented GetNumber Call

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,25 +1,32 @@
 ﻿using System;
+using System.Text.Json;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using csi_media_test.Models;
+using csi_media_test.Data;
 
 namespace csi_media_test.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly csiDBContext _dbContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, csiDBContext context)
         {
             _logger = logger;
+            _dbContext = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var data = await GetNumberService();
+            ViewData["numbers"] = data;
             return View();
         }
 
